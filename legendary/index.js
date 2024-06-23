879e15330160ff831efc5bdf5a17550701bab0b2
Thaynison <cthaynison@outlook.com.br>
2024-06-23 01:55:11 -0300

    Discord Bot
    Adição do discord bot

--- legendary/index.js
+++ legendary/index.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
-const Discord = require("discord.js");
-const express = require("express");
-const path = require("path");
-
+const Discord = require("discord.js")
 const client = new Discord.Client({
     intents: [
         32767, Discord.GatewayIntentBits.GuildMessages,
@@ -10,34 +7,22 @@ const client = new Discord.Client({
     ]
 });
 
-client.slashCommands = new Discord.Collection();
-require('./handler')(client);
+module.exports = client
 
 client.on("interactionCreate", async (interaction) => {
-    if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+    if(interaction.type === Discord.InteractionType.ApplicationCommand){
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd) return interaction.reply(`Error`);
         interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-        cmd.run(client, interaction);
+        cmd.run(client,interaction)
     }
-});
+})
+
+client.slashCommands = new Discord.Collection()
+require('./handler')(client)
 
 client.on("ready", async () => {
-    console.log("Olá estou online!");
-});
+    console.log("Olá estou online!")
+})
 
-client.login(process.env.TOKEN_BOT);
-
-// Configuração do servidor Express
-const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.render('index', { bot: client });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+client.login(process.env.TOKEN_BOT)
