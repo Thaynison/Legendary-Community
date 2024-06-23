9db71052500831c244c34492563e95aba22a6875
Thaynison <cthaynison@outlook.com.br>
2024-06-23 02:59:49 -0300

    att

--- legendary/index.js
+++ legendary/index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
-const Discord = require("discord.js");
-const express = require("express");
-
+const Discord = require("discord.js")
 const client = new Discord.Client({
     intents: [
         32767, Discord.GatewayIntentBits.GuildMessages,
@@ -9,33 +7,22 @@ const client = new Discord.Client({
     ]
 });
 
+module.exports = client
+
 client.on("interactionCreate", async (interaction) => {
-    if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+    if(interaction.type === Discord.InteractionType.ApplicationCommand){
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd) return interaction.reply(`Error`);
         interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-        cmd.run(client, interaction);
+        cmd.run(client,interaction)
     }
-});
+})
 
-client.slashCommands = new Discord.Collection();
-require('./handler')(client);
+client.slashCommands = new Discord.Collection()
+require('./handler')(client)
 
 client.on("ready", async () => {
-    console.log("Olá, estou online!");
-});
+    console.log("Olá estou online!")
+})
 
-client.login(process.env.TOKEN_BOT);
-
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-    res.send("Bot de Discord está rodando!");
-});
-
-app.listen(port, () => {
-    console.log(`Servidor Express rodando na porta ${port}`);
-});
-
-module.exports = app;
+client.login(process.env.TOKEN_BOT)
