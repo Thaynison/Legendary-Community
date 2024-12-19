549add2ee298a4491a0dbccce5ae7c663d426814
Thaynison <cthaynison@outlook.com.br>
2024-12-18 22:17:50 -0300

    Update open_list_ticket.js

--- website/assets/js/open_list_ticket.js
+++ website/assets/js/open_list_ticket.js
@@ -134,7 +134,7 @@ function changeContent(contentType) {
                         <textarea id="descricao" name="descricao" placeholder="Informar Descrição do Ticket" required></textarea>
                     </div>
                     <div class="form-field">
-                        <a href="https://imgur.com/"<i class="fas fa-gamepad"></i> Site para Upar Foto</a>
+                        <a href="https://imgur.com/"><i class="fas fa-gamepad"></i> Site para upar foto</a>
                         <label for="print">Anexar Print:</label>
                         <input type="url" id="print" name="print" placeholder="Anexar Print" required>
                     </div>
