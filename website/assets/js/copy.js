4ad4f432258d1e7008afc30d246bda1fd0645611
Thaynison <cthaynison@outlook.com.br>
2024-12-21 03:22:04 -0300

    att

--- website/assets/js/copy.js
+++ website/assets/js/copy.js
@@ -1,13 +1,33 @@
-function copyToClipboard() {
-    var input = document.getElementById("ip");
-    input.select();
-    input.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    alert("IP copiado: " + input.value);
-}
+// Inserindo CSS via JavaScript
+const style = document.createElement("style");
+style.innerHTML = `
+    body {
+        user-select: none; /* Bloqueia seleção de texto */
+        -webkit-user-select: none; /* Compatibilidade com navegadores baseados em WebKit */
+    }
+`;
+document.head.appendChild(style);
 
-document.addEventListener("copy", function (event) {
-    if (event.target.id !== "ip") {
-        event.preventDefault();
+// Bloqueia teclas padrão de copiar e botão direito
+document.addEventListener('keydown', function (e) {
+    if ((e.ctrlKey && e.key === 'c') || (e.ctrlKey && e.key === 'x')) {
+        e.preventDefault();
     }
 });
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Permite cópia apenas via função
+function copyToClipboard() {
+    var input = document.createElement("textarea");
+    input.value = document.getElementById("ip").textContent;
+
+    document.body.appendChild(input);
+    input.select();
+    input.setSelectionRange(0, 99999);
+
+    document.execCommand("copy");
+    document.body.removeChild(input);
+}
