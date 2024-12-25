649a77d85a94783d08977ab2fcd103453bc61986
Thaynison <cthaynison@outlook.com.br>
2024-12-24 21:40:10 -0300

    att

--- website/assets/js/painel_staff.js
+++ website/assets/js/painel_staff.js
@@ -1381,7 +1381,7 @@ function changeContent(contentType) {
 
             const fetchTicketDate = async (ticketId) => {
                 const dataInput = document.getElementById('data');
-                dataInput.value = '0000-00-00000:00';
+                dataInput.value = 'Carregando data...';
                 try {
                     const response = await fetch(`https://dash.legendarycommunity.com.br/api/api_tickets_id.php?id_ticket=${ticketId}`);
                     if (!response.ok) {
@@ -1397,14 +1397,14 @@ function changeContent(contentType) {
                             const formattedDate = new Date(ticketDate).toISOString().slice(0, 16);
                             dataInput.value = formattedDate;
                         } else {
-                            dataInput.value = '0000-00-00000:00';
+                            dataInput.value = 'Nenhuma data disponível.';
                         }
                     } else {
-                        dataInput.value = '0000-00-00000:00';
+                        dataInput.value = 'Nenhuma data encontrada.';
                     }
                 } catch (error) {
                     console.error('Erro ao carregar data do ticket:', error);
-                    dataInput.value = '0000-00-00000:00';
+                    dataInput.value = 'Erro ao carregar data.';
                 }
             };
             
