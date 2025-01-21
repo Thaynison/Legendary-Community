365cdd953daa34d8d75360ab25bd810762bff801
Thaynison <cthaynison@outlook.com.br>
2025-01-21 13:58:22 -0300

    att

--- website/assets/js/modpack.js
+++ website/assets/js/modpack.js
@@ -31,20 +31,3 @@ document.getElementById('downloadBtnIndustrial').addEventListener('click', funct
             alert('Erro ao obter a última release.');
         });
 });
-
-document.getElementById('downloadBtnTextureSurvival').addEventListener('click', function() {
-    fetch('https://api.github.com/repos/Thaynison/LegendaryTexture/releases/latest')
-        .then(response => response.json())
-        .then(data => {
-            const asset = data.assets[0];
-            if (asset) {
-                window.location.href = asset.browser_download_url;
-            } else {
-                alert('Nenhum arquivo disponível para download.');
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao obter a última release:', error);
-            alert('Erro ao obter a última release.');
-        });
-});
